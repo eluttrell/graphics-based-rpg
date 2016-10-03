616687b45bc11376566e7ed5584d9ab829398974
Elijah <elieel@gmail.com>
2016-10-03 16:04:31 -0400

    Add hero monster collision detection.

--- catch-a-monster.py
+++ catch-a-monster.py
@@ -1,7 +1,39 @@
 import pygame
 import random
 import time
-import threading
+import math
+
+KEY_UP = 273
+KEY_DOWN = 274
+KEY_LEFT = 276
+KEY_RIGHT = 275
+
+
+class Hero(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.x_speed = 0
+        self.y_speed = 0
+
+    def render(self, screen):
+        hero_image = pygame.image.load('images/hero.png').convert_alpha()
+        screen.blit(hero_image, (self.x, self.y))
+
+    def stay_on_screen(self):
+        if self.x >= 448:
+            self.x = 448
+        if self.x <= 32:
+            self.x = 32
+        if self.y >= 408:
+            self.y = 408
+        if self.y <= 32:
+            self.y = 32
+
+    def movement(self):
+        self.x += self.x_speed
+        self.y += self.y_speed
 
 
 class Monster(object):
@@ -9,19 +41,19 @@ class Monster(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.x_speed = 3
-        self.y_speed = 3
+        self.x_speed = 6
+        self.y_speed = 6
 
     def render(self, screen):
         monster_image = pygame.image.load('images/monster.png').convert_alpha()
         screen.blit(monster_image, (self.x, self.y))
 
-    def stay_on_screen(self, width, height):
-        if self.x > width:
+    def stay_on_screen(self):
+        if self.x > 512:
             self.x = 0
         if self.x < 0:
             self.x = 512
-        if self.y > height:
+        if self.y > 480:
             self.y = 0
         if self.y < 0:
             self.y = 480
@@ -31,31 +63,32 @@ class Monster(object):
         self.y += self.y_speed
 
     def change_direction(self):
+        x = 6
         direction = random.randint(0, 8)
         if direction == 0:
-            self.x_speed = 2
+            self.x_speed = x
             self.y_speed = 0
         elif direction == 1:
-            self.x_speed = -2
+            self.x_speed = -x
             self.y_speed = 0
         elif direction == 2:
-            self.y_speed = 2
+            self.y_speed = x
             self.x_speed = 0
         elif direction == 3:
-            self.y_speed = -2
+            self.y_speed = -x
             self.x_speed = 0
         elif direction == 4:
-            self.y_speed = 2
-            self.x_speed = 2
+            self.y_speed = x
+            self.x_speed = x
         elif direction == 5:
-            self.y_speed = -2
-            self.x_speed = -2
+            self.y_speed = -x
+            self.x_speed = -x
         elif direction == 6:
-            self.y_speed = 2
-            self.x_speed = -2
+            self.y_speed = x
+            self.x_speed = -x
         else:
-            self.y_speed = -2
-            self.x_speed = 2
+            self.y_speed = -x
+            self.x_speed = x
 
 
 def main():
@@ -80,19 +113,13 @@ def main():
     # PUT INITIALIZATION CODE HERE #
     ################################
 
+    # monster stuff
     change_dir_countdown = 50
-
-    # monster_image = pygame.image.load('images/monster.png').convert_alpha()
-
     monster = Monster(345, 180)
 
-    hero_image = pygame.image.load('images/hero.png').convert_alpha()
-
-    # monster_x = 345
-    # monster_y = 180
-
-    # monster_x_speed = 0
-    # monster_y_speed = 0
+    # hero stuff
+    # hero_image = pygame.image.load('images/hero.png').convert_alpha()
+    hero = Hero(240, 230)
 
     # game loop
     stop_game = False
@@ -106,10 +133,34 @@ def main():
                 # if they closed the window, set stop_game to True
                 # to exit the main loop
                 stop_game = True
+            if event.type == pygame.KEYDOWN:
+                # activate the cooresponding speeds
+                # when an arrow key is pressed down
+                if event.key == KEY_DOWN:
+                    hero.y_speed = 3
+                elif event.key == KEY_UP:
+                    hero.y_speed = -3
+                elif event.key == KEY_LEFT:
+                    hero.x_speed = -3
+                elif event.key == KEY_RIGHT:
+                    hero.x_speed = 3
+            if event.type == pygame.KEYUP:
+                # deactivate the cooresponding speeds
+                # when an arrow key is released
+                if event.key == KEY_DOWN:
+                    hero.y_speed = 0
+                elif event.key == KEY_UP:
+                    hero.y_speed = 0
+                elif event.key == KEY_LEFT:
+                    hero.x_speed = 0
+                elif event.key == KEY_RIGHT:
+                    hero.x_speed = 0
 
         #######################################
         # PUT LOGIC TO UPDATE GAME STATE HERE #
         #######################################
+
+        hero.movement()
 
         change_dir_countdown -= 1
 
@@ -124,41 +175,18 @@ def main():
         # PUT CUSTOM DISPLAY CODE HERE #
         ################################
 
-        screen.blit(hero_image, (240, 230))
-        # screen.blit(monster_image, (monster_x, monster_y))
+        # hero stuff
+        # screen.blit(hero_image, (240, 230))
+        hero.render(screen)
+        hero.stay_on_screen()
+
+        # monster stuff
         monster.render(screen)
-        monster.stay_on_screen(512, 480)
+        monster.stay_on_screen()
         monster.monster_move()
         if change_dir_countdown == 0:
             monster.change_direction()
             change_dir_countdown = 50
-        # if monster_x > width:
-        #     monster_x = 0
-        # if monster_x < 0:
-        #     monster_x = 512
-        # if monster_y > height:
-        #     monster_y = 0
-        # if monster_y < 0:
-        #     monster_y = 480
-
-        # monster_x += monster_x_speed
-        # monster_y += monster_y_speed
-
-        # if change_dir_countdown == 0:
-        #     change_dir_countdown = 50
-        #     direction = random.randint(0, 3)
-        #     if direction == 0:
-        #         monster_x_speed = 2
-        #         monster_y_speed = 0
-        #     elif direction == 1:
-        #         monster_x_speed = -2
-        #         monster_y_speed = 0
-        #     elif direction == 2:
-        #         monster_y_speed = 2
-        #         monster_x_speed = 0
-        #     else:
-        #         monster_y_speed = -2
-        #         monster_x_speed = 0
 
         # update the canvas display with the currently drawn frame
         pygame.display.update()
