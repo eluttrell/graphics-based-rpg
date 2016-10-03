91063f1766138068672a9d6ad268bc17251d3b70
Elijah <elieel@gmail.com>
2016-10-03 13:02:05 -0400

    Add Monster and Hero images. Make monster move to the right, east. Make monster loop back to left (west) side of screen when it travels too far east.

--- catch-a-monster.py
+++ catch-a-monster.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+import time
+import threading
 
 
 def main():
@@ -23,6 +26,15 @@ def main():
     # PUT INITIALIZATION CODE HERE #
     ################################
 
+    monster_image = pygame.image.load('images/monster.png').convert_alpha()
+
+    hero_image = pygame.image.load('images/hero.png').convert_alpha()
+
+    monster_x = 345
+    monster_y = 180
+
+    loop_counter = 0
+
     # game loop
     stop_game = False
     while not stop_game:
@@ -40,7 +52,7 @@ def main():
         # PUT LOGIC TO UPDATE GAME STATE HERE #
         #######################################
 
-        hero_image = pygame.image.load('images/hero.png').convert_alpha()
+        monster_x += 2
 
         # initialize pygame.display for bacground image
         background_image = pygame.image.load(
@@ -53,13 +65,17 @@ def main():
         # PUT CUSTOM DISPLAY CODE HERE #
         ################################
 
-        screen.blit(hero_image, (240, 240))
+        screen.blit(hero_image, (240, 230))
+        screen.blit(monster_image, (monster_x, monster_y))
+        if monster_x > width:
+            monster_x = 0
 
         # update the canvas display with the currently drawn frame
         pygame.display.update()
 
         # tick the clock to enforce a max framerate
         clock.tick(60)
+        loop_counter += 1
 
     # quit pygame properly to clean up resources
     pygame.quit()
