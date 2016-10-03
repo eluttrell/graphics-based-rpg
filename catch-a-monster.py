2d80d0a085b5072e0699725c8f693871d0fa627a
Elijah <elieel@gmail.com>
2016-10-03 13:43:52 -0400

    Make monster randomly change direction to either up (n), down (s), left (we), or right (e).

--- catch-a-monster.py
+++ catch-a-monster.py
@@ -6,8 +6,8 @@ import threading
 
 def main():
     # declare the size of the canvas
-    width = 500
-    height = 500
+    width = 512
+    height = 480
     blue_color = (97, 159, 182)
 
     # initialize the pygame framework
@@ -26,6 +26,8 @@ def main():
     # PUT INITIALIZATION CODE HERE #
     ################################
 
+    change_dir_countdown = 120
+
     monster_image = pygame.image.load('images/monster.png').convert_alpha()
 
     hero_image = pygame.image.load('images/hero.png').convert_alpha()
@@ -34,6 +36,9 @@ def main():
     monster_y = 180
 
     loop_counter = 0
+
+    monster_x_speed = 0
+    monster_y_speed = 0
 
     # game loop
     stop_game = False
@@ -52,7 +57,7 @@ def main():
         # PUT LOGIC TO UPDATE GAME STATE HERE #
         #######################################
 
-        monster_x += 2
+        change_dir_countdown -= 1
 
         # initialize pygame.display for bacground image
         background_image = pygame.image.load(
@@ -69,13 +74,38 @@ def main():
         screen.blit(monster_image, (monster_x, monster_y))
         if monster_x > width:
             monster_x = 0
+        if monster_x < 0:
+            monster_x = 512
+        if monster_y > height:
+            monster_y = 0
+        if monster_y < 0:
+            monster_y = 480
+
+        monster_x += monster_x_speed
+        monster_y += monster_y_speed
+
+        print change_dir_countdown
+        if change_dir_countdown == 0:
+            change_dir_countdown = 120
+            direction = random.randint(0, 3)
+            if direction == 0:
+                monster_x_speed = 2
+                monster_y_speed = 0
+            elif direction == 1:
+                monster_x_speed = -2
+                monster_y_speed = 0
+            elif direction == 2:
+                monster_y_speed = 2
+                monster_x_speed = 0
+            else:
+                monster_y_speed = -2
+                monster_x_speed = 0
 
         # update the canvas display with the currently drawn frame
         pygame.display.update()
 
         # tick the clock to enforce a max framerate
         clock.tick(60)
-        loop_counter += 1
 
     # quit pygame properly to clean up resources
     pygame.quit()
